1e28df6cc90f5ff186bff4362b65a403d73b52f8
Fox455664 <118382354+Fox455664@users.noreply.github.com>
2026-02-12 01:46:12 +0200

    Delete cuddle-whisper-craft-main directory

--- cuddle-whisper-craft-main/src/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://puyugteuduuuvjijurjh.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1eXVndGV1ZHV1dXZqaWp1cmpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3MjMxMzMsImV4cCI6MjA4NTI5OTEzM30.wZpkRxuEt7aa4Qc5nxWLAAAVOski7qAJzHhjLvWhIvg';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
